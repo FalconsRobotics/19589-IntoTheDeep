22f2f9938567a0e863e54a59e0680d26f24b498d
EdwinDick <144410146+edwindick@users.noreply.github.com>
2024-10-01 15:38:57 -0700

    Something went weird - Restoring

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/DriveBaseTest.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode.tests;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.subsystems.DriveBase;
+
+@TeleOp
+public class DriveBaseTest extends LinearOpMode {
+    public void runOpMode() {
+        final DriveBase driveBase = new DriveBase(hardwareMap);
+        waitForStart();
+
+        while (opModeIsActive() == true) {
+            driveBase.setVelocity(gamepad1.left_stick_y, -gamepad1.left_stick_x, -gamepad1.right_stick_x, 1);
+        }
+
+        driveBase.finalize();
+    }
+}
