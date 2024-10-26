eb2c500ee2499268141534523d7080ccba3639d4
Andrew Bota <andrewcbota@icloud.com>
2024-10-26 11:31:43 -0700

    Added documentation and rotating functions to Intake. Created an IntakeTest OpMode for testing Intake subsystem.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tests/IntakeTest.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode.opmodes.tests;
+
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.subsystems.SubsystemsCollection;
+
+@TeleOp(name = "Test - Intake")
+public class IntakeTest extends LinearOpMode {
+    public void runOpMode() {
+        SubsystemsCollection sys = SubsystemsCollection.getInstance(hardwareMap);
+        GamepadEx gamepad1 = new GamepadEx(this.gamepad1);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            sys.intake.moveSlides((gamepad1.getLeftX() + 1.0) / 2);
+
+            if (gamepad1.gamepad.dpad_left) {
+                sys.intake.rotateToHandoff();
+            } else if (gamepad1.gamepad.dpad_up) {
+                sys.intake.rotateToHover();
+            } else if (gamepad1.gamepad.dpad_right) {
+                sys.intake.rotateToPickup();
+            }
+
+            telemetry.addData("Left Extension Position", sys.intake.leftExtensionServo.getPosition());
+            telemetry.addData("Right Extension Position", sys.intake.leftExtensionServo.getPosition());
+            telemetry.addData("Pivoting Motor Position", sys.intake.pivotingArmMotor.getCurrentPosition());
+            telemetry.update();
+        }
+    }
+
+
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
@@ -10,13 +10,32 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 public class Intake extends SubsystemBase {
-    public final ServoEx leftExtensionServo;
-    public final ServoEx rightExtensionServo;
+    public static class SlidePositions {
+        public static double RETRACTED = 0.0;
+        public static double EXTENDED = 100.0;
+    }
 
+    public static class ArmPositions {
+        public static int EXTAKE = 0;
+        public static int HOVER = 0;
+        public static int PICKUP = 0;
+
+    }
+
+    /**
+     * Servo object for the left and right linear linkage extension servos.
+     */
+    public final ServoEx leftExtensionServo, rightExtensionServo;
+
+    /**
+     * Motor object for the pivoting arm.
+     */
     public final MotorEx pivotingArmMotor;
 
-    public final ServoEx frontIntakeServo;
-    public final ServoEx backIntakeServo;
+    /**
+     * Servo object for the front and back wheels.
+     */
+    public final ServoEx frontIntakeServo, backIntakeServo;
 
     public Intake(HardwareMap map) {
         leftExtensionServo = new SimpleServo(map, "Intake-LeftExtension", 0, 360, AngleUnit.DEGREES);
@@ -26,6 +45,36 @@ public class Intake extends SubsystemBase {
 
         frontIntakeServo = new SimpleServo(map, "Intake-FrontIntakeWheel", 0, 360, AngleUnit.DEGREES);
         backIntakeServo = new SimpleServo(map, "Intake-BackIntakeWheel", 0, 360, AngleUnit.DEGREES);
+    }
+
+    /**
+     * Sets the positions of both slide servos.
+     * @param position Sets the positions of both servos.
+     */
+    public void moveSlides(double position) {
+        leftExtensionServo.setPosition(position);
+        rightExtensionServo.setPosition(position);
+    }
+
+    /**
+     * Sets the pivoting motor to the predefined extake position.
+     */
+    public void rotateToHandoff() {
+        pivotingArmMotor.setTargetPosition(ArmPositions.EXTAKE);
+    }
+
+    /**
+     * Sets the pivoting motor to the predefined hover position.
+     */
+    public void rotateToHover() {
+        pivotingArmMotor.setTargetPosition(ArmPositions.HOVER);
+    }
+
+    /**
+     * Sets the pivoting motor to the predefined pickup position.
+     */
+    public void rotateToPickup() {
+        pivotingArmMotor.setTargetPosition(ArmPositions.PICKUP);
     }
 
     public void periodic() {
