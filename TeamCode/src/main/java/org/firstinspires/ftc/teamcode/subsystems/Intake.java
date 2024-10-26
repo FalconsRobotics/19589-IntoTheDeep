4372d95b7fa4064b22b0a0fd731fb98486ed3284
Andrew Bota <andrewcbota@icloud.com>
2024-10-26 10:18:13 -0700

    2024-10-26 Added base Intake hardware.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
@@ -1,18 +1,31 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
 import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.arcrobotics.ftclib.hardware.SimpleServo;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.arcrobotics.ftclib.hardware.motors.MotorEx;
+import com.arcrobotics.ftclib.hardware.ServoEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.Servo;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 public class Intake extends SubsystemBase {
-    public final Servo slide = null;
-    public final Motor arm = null;
+    public final ServoEx leftExtensionServo;
+    public final ServoEx rightExtensionServo;
+
+    public final MotorEx pivotingArmMotor;
+
+    public final ServoEx frontIntakeServo;
+    public final ServoEx backIntakeServo;
 
     public Intake(HardwareMap map) {
-        // slide = map.get(Servo.class, "Intake-Slide");
-        // arm = new Motor(map, "Intake-Arm");
+        leftExtensionServo = new SimpleServo(map, "Intake-LeftExtension", 0, 360, AngleUnit.DEGREES);
+        rightExtensionServo = new SimpleServo(map, "Intake-LeftExtension", 0, 360, AngleUnit.DEGREES);
+
+        pivotingArmMotor = new MotorEx(map, "Intake-PivotingArm", Motor.GoBILDA.RPM_84);
+
+        frontIntakeServo = new SimpleServo(map, "Intake-FrontIntakeWheel", 0, 360, AngleUnit.DEGREES);
+        backIntakeServo = new SimpleServo(map, "Intake-BackIntakeWheel", 0, 360, AngleUnit.DEGREES);
     }
 
     public void periodic() {
