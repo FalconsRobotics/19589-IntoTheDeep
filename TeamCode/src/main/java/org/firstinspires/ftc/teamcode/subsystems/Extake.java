7e95f18b19b6b26c260f4563624e667be01bab92
Edwin Dick <edwindick23@gmail.com>
2024-10-28 17:57:04 -0700

    Extake cleanup.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Extake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Extake.java
@@ -9,19 +9,30 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 public class Extake extends SubsystemBase {
+    public static class ArmPosition {
+        public static double LOAD = 0.94;
+        public static double UNLOAD = 0.31;
+    }
+
+    public static class LiftPosition {
+        public static int DOWM = 0;
+    }
+
     /** Motor controlling tube slide. */
-    public final MotorEx slide;
+    public final MotorEx lift;
 
     /** Left and right servos controlling the bucket arm. */
     public final SimpleServo leftArm, rightArm;
 
     public Extake(HardwareMap map) {
-        slide = new MotorEx(map, "Extake-Slide", Motor.GoBILDA.RPM_435);
-        slide.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
-        slide.setRunMode(Motor.RunMode.PositionControl);
+        lift = new MotorEx(map, "Extake-Slide", Motor.GoBILDA.RPM_435);
+        lift.setRunMode(Motor.RunMode.PositionControl);
+        lift.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
+        lift.setTargetPosition(LiftPosition.DOWM);
 
         leftArm = new SimpleServo(map, "Extake-LeftArm", 0, 360, AngleUnit.DEGREES);
         rightArm = new SimpleServo(map, "Extake-RightArm", 0, 360, AngleUnit.DEGREES);
+        setArmPosition(ArmPosition.LOAD);
         // Servo face opposite directions, thankfully this is addressed with servo programming.
     }
 
@@ -30,7 +41,7 @@ public class Extake extends SubsystemBase {
     }
 
     /** Sets `rotation` of both arm servos. */
-    public void setArmRotation(double rotation) {
+    public void setArmPosition(double rotation) {
         leftArm.setPosition(rotation);
         rightArm.setPosition(rotation);
     }
