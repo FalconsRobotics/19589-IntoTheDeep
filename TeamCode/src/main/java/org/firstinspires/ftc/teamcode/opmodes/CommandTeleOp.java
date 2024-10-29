406044caadedd5874e791c4b403f8923be746b07
Edwin Dick <edwindick23@gmail.com>
2024-10-29 16:13:45 -0700

    Some drive base controls. Unfinished.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/CommandTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/CommandTeleOp.java
@@ -1,6 +1,8 @@
 package org.firstinspires.ftc.teamcode.opmodes;
 
+import com.arcrobotics.ftclib.command.Command;
 import com.arcrobotics.ftclib.command.CommandOpMode;
+import com.arcrobotics.ftclib.command.Subsystem;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.arcrobotics.ftclib.gamepad.GamepadKeys;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -10,19 +12,37 @@ import org.firstinspires.ftc.teamcode.opmodes.commands.CommandExtakeMoveLift;
 import org.firstinspires.ftc.teamcode.subsystems.Extake;
 import org.firstinspires.ftc.teamcode.subsystems.SubsystemsCollection;
 
+import java.util.Collections;
+import java.util.Set;
+
 @TeleOp(name = "Command TeleOp")
 public class CommandTeleOp extends CommandOpMode {
     private SubsystemsCollection sys;
     private GamepadEx driverGamepad, utilityGamepad;
+
+    // For finer drive base control.
+    private double rotationMultiplier = 1.0, speedMultiplier = 1.0;
 
     public void initialize() {
         sys = SubsystemsCollection.getInstance(hardwareMap);
         driverGamepad = new GamepadEx(gamepad1);
         utilityGamepad = new GamepadEx(gamepad2);
 
+        // Drive Base commands
+        sys.driveBase.brake(driverGamepad.getButton(GamepadKeys.Button.LEFT_BUMPER));
+
         schedule(new CommandDriveBaseDriveRobotCentric(
-                () -> driverGamepad.getLeftY(), () -> driverGamepad.getLeftX(),
-                () -> driverGamepad.getRightX()
+                () -> (driverGamepad.getLeftY() +
+                        (driverGamepad.getButton(GamepadKeys.Button.DPAD_UP) ? 1.0 : 0.0) -
+                        (driverGamepad.getButton(GamepadKeys.Button.DPAD_DOWN) ? 1.0 : 0.0)) *
+                        speedMultiplier,
+
+                () -> (driverGamepad.getLeftX() +
+                        (driverGamepad.getButton(GamepadKeys.Button.DPAD_RIGHT) ? 1.0 : 0.0) -
+                        (driverGamepad.getButton(GamepadKeys.Button.DPAD_LEFT) ? 1.0 : 0.0)) *
+                        speedMultiplier,
+
+                () -> driverGamepad.getRightX() * rotationMultiplier
         ));
     }
 }
