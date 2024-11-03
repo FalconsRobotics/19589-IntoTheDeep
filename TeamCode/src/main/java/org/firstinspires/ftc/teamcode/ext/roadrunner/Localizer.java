ecb53a5e2c256db968a2bf118a205676a290b072
Edwin Dick <edwindick23@gmail.com>
2024-11-03 01:01:51 -0700

    Road Runner library and quickstart files added to project.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ext/roadrunner/Localizer.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.ext.roadrunner;
+
+import com.acmerobotics.roadrunner.Time;
+import com.acmerobotics.roadrunner.Twist2dDual;
+
+public interface Localizer {
+    Twist2dDual<Time> update();
+}
