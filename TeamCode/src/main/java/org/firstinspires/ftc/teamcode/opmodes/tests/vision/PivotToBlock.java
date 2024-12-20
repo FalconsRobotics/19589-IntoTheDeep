5a708042c888c649124d71d80f69b76d7fa35748
Teigan Bronec <teiganbronec1@gmail.com>
2024-12-19 17:19:16 -0800

    Added changes to pivot to block

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tests/vision/PivotToBlock.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tests/vision/PivotToBlock.java
@@ -2,14 +2,28 @@ package org.firstinspires.ftc.teamcode.opmodes.tests.vision;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.commands.CommandIntakeSetPivot;
+import org.firstinspires.ftc.teamcode.subsystems.Intake;
+import org.firstinspires.ftc.teamcode.utilities.roadrunner.VisionControlUtility;
 
 //import org.firstinspires.ftc.teamcode.utilities.VisionControlUtility;
 
 @TeleOp(name = "Pivot to Block Test", group = "Tests")
 public class PivotToBlock extends LinearOpMode {
-//    VisionControlUtility vision = VisionControlUtility.getInstance(hardwareMap);
+       VisionControlUtility vision = new VisionControlUtility(hardwareMap);
 
     public void runOpMode() {
-//        if(VisionControlUtility.processCorners(vision.limelight.get))
+        waitForStart();
+        while (opModeIsActive()) {
+            if(VisionControlUtility.processCorners() <= 20){
+                new CommandIntakeSetPivot(Intake.PivotPosition.MIDDLE);
+            } else if(VisionControlUtility.processCorners() > 20 && VisionControlUtility.processCorners() < 45){
+                new CommandIntakeSetPivot(Intake.PivotPosition.LEFT);
+            } else if(VisionControlUtility.processCorners() >= 45){
+                new CommandIntakeSetPivot(Intake.PivotPosition.LEFT);
+            }
+        }
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilities/roadrunner/VisionControlUtility.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilities/roadrunner/VisionControlUtility.java
@@ -3,49 +3,53 @@ package org.firstinspires.ftc.teamcode.utilities.roadrunner;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-import org.firstinspires.ftc.teamcode.utilities.SubsystemsCollection;
-
 import java.util.List;
 
 public class VisionControlUtility {
 
-     private static VisionControlUtility instance = null;
-     private HardwareMap map = null;
+     public static Limelight3A limelight;
 
-     Limelight3A limelight;
-
-     private VisionControlUtility(HardwareMap map) {
-          this.map = map;
-
+     public VisionControlUtility(HardwareMap map) {
           limelight = map.get(Limelight3A.class, "limelight");
+          limelight.pipelineSwitch(0);
      }
 
-
-     //List<double[]> corners = limelight.targetCorners();
-     double x1, y1;
+     List<Double> x1;
+     List<Double> y1;
      double x2, y2;
      double x3, y3;
 
-     public double findLength(double x1, double y1, double x2, double y2) {
+     static List<List<Double>> corners = limelight.getLatestResult().getColorResults().get(0).getTargetCorners();
+
+
+     public static double findLength(double x1, double y1, double x2, double y2) {
           return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
      }
 
-     double l1 = findLength(x1, y1, x2, y2);
-     double l2 = findLength(x2, y2, x3, y3);
 
-     public double findAngle(double x1, double y1, double x2, double y2) {
+
+     public static double findAngle(double x1, double y1, double x2, double y2) {
           return Math.toDegrees(Math.atan2(y2 - y1, x2 - x1));
      }
 
-     public double returnAngle() {
-          if (l1 > l2) {
-               findAngle(x1, y1, x2, y2);
+     public static double processCorners(){
+         List<Double> corner1 = corners.get(0);
+         List<Double> corner2 = corners.get(1);
+         List<Double> corner3 = corners.get(2);
+
+
+
+          double l1 = findLength(corner1.get(0), corner1.get(1), corner2.get(0), corner2.get(1));
+          double l2 = findLength(corner2.get(0), corner2.get(1), corner3.get(0), corner3.get(1));
+
+          if(l1 > l2){
+              return findAngle(corner1.get(0), corner1.get(1), corner2.get(0), corner2.get(1));
           } else {
-               findAngle(x2, y2, x3, y3);
+              return findAngle(corner2.get(0), corner2.get(1), corner3.get(0), corner3.get(1));
           }
 
-          return 0.0;
      }
+
 }
 
 
