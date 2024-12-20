f348667ef92b06a0513017a6e458e1a7d838a5bd
Teigan Bronec <teiganbronec1@gmail.com>
2024-12-20 12:33:38 -0800

    Made some changes to the VisionControlUtility and PivotToBlock OpMode. Should work now gonna test later.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tests/vision/PivotToBlock.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tests/vision/PivotToBlock.java
@@ -2,13 +2,10 @@ package org.firstinspires.ftc.teamcode.opmodes.tests.vision;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.commands.CommandIntakeSetPivot;
 import org.firstinspires.ftc.teamcode.subsystems.Intake;
 import org.firstinspires.ftc.teamcode.utilities.roadrunner.VisionControlUtility;
-
-//import org.firstinspires.ftc.teamcode.utilities.VisionControlUtility;
 
 @TeleOp(name = "Pivot to Block Test", group = "Tests")
 public class PivotToBlock extends LinearOpMode {
@@ -17,11 +14,11 @@ public class PivotToBlock extends LinearOpMode {
     public void runOpMode() {
         waitForStart();
         while (opModeIsActive()) {
-            if(VisionControlUtility.processCorners() <= 20){
+            if(vision.processCorners() <= 20){
                 new CommandIntakeSetPivot(Intake.PivotPosition.MIDDLE);
-            } else if(VisionControlUtility.processCorners() > 20 && VisionControlUtility.processCorners() < 45){
+            } else if(vision.processCorners() > 20 && vision.processCorners() < 45){
                 new CommandIntakeSetPivot(Intake.PivotPosition.LEFT);
-            } else if(VisionControlUtility.processCorners() >= 45){
+            } else if(vision.processCorners() >= 45){
                 new CommandIntakeSetPivot(Intake.PivotPosition.LEFT);
             }
         }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilities/roadrunner/VisionControlUtility.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilities/roadrunner/VisionControlUtility.java
@@ -8,36 +8,26 @@ import java.util.List;
 public class VisionControlUtility {
 
      public static Limelight3A limelight;
-
+     static List<List<Double>> corners;
      public VisionControlUtility(HardwareMap map) {
           limelight = map.get(Limelight3A.class, "limelight");
           limelight.pipelineSwitch(0);
      }
 
-     List<Double> x1;
-     List<Double> y1;
-     double x2, y2;
-     double x3, y3;
 
-     static List<List<Double>> corners = limelight.getLatestResult().getColorResults().get(0).getTargetCorners();
-
-
-     public static double findLength(double x1, double y1, double x2, double y2) {
+    public static double findLength(double x1, double y1, double x2, double y2) {
           return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
      }
-
-
 
      public static double findAngle(double x1, double y1, double x2, double y2) {
           return Math.toDegrees(Math.atan2(y2 - y1, x2 - x1));
      }
 
-     public static double processCorners(){
+     public double processCorners(){
+         corners = limelight.getLatestResult().getColorResults().get(0).getTargetCorners();
          List<Double> corner1 = corners.get(0);
          List<Double> corner2 = corners.get(1);
          List<Double> corner3 = corners.get(2);
-
-
 
           double l1 = findLength(corner1.get(0), corner1.get(1), corner2.get(0), corner2.get(1));
           double l2 = findLength(corner2.get(0), corner2.get(1), corner3.get(0), corner3.get(1));
