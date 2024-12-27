5e8c78f6cd09a2b402626db44ef387f66f581add
Teigan Bronec <teiganbronec1@gmail.com>
2024-12-26 22:30:41 -0800

    Finished the utility for finding the angle of the samples and made an opmode to test it.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tests/vision/PivotToBlock.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tests/vision/PivotToBlock.java
@@ -2,26 +2,47 @@ package org.firstinspires.ftc.teamcode.opmodes.tests.vision;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.arcrobotics.ftclib.command.CommandOpMode;
+import com.arcrobotics.ftclib.command.ParallelCommandGroup;
+import com.arcrobotics.ftclib.command.ParallelDeadlineGroup;
+import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.teamcode.commands.*;
+import org.firstinspires.ftc.teamcode.utilities.DeltaTime;
+import org.firstinspires.ftc.teamcode.utilities.SubsystemsCollection;
+import org.firstinspires.ftc.teamcode.subsystems.Intake;
+import org.firstinspires.ftc.teamcode.subsystems.Extake;
+
+import org.firstinspires.ftc.teamcode.subsystems.Intake;
 import org.firstinspires.ftc.teamcode.utilities.vision.VisionUtility;
 
 @TeleOp(name = "Pivot to Block Test", group = "Tests")
 public class PivotToBlock extends LinearOpMode {
-    VisionUtility vision = new VisionUtility(hardwareMap);
-
     public void runOpMode() {
+        VisionUtility vision = new VisionUtility(hardwareMap);
         waitForStart();
 
         while (opModeIsActive()) {
             double angle = vision.findBlockAngle();
 
-            if(angle <= 20) {
-                telemetry.addData("Angle (<= 20)", angle);
-            } else if(angle > 20 && angle < 45) {
-                telemetry.addData("Angle (> 20; < 45)", angle);
-            } else if(angle >= 45) {
-                telemetry.addData("Angle (>= 45)", angle);
+            if(angle <= 6 || angle >= 170) {
+                new CommandIntakeSetPivot(Intake.PivotPosition.MIDDLE);
+                telemetry.addData("Angle: ", angle);
+            } else if(angle > 6 && angle <= 80) {
+                new CommandIntakeSetPivot(Intake.PivotPosition.LEFT);
+                telemetry.addData("Angle: ", angle);
+            } else if(angle > 80 && angle <= 120) {
+                new CommandIntakeSetPivot(Intake.PivotPosition.RIGHT);
+                telemetry.addData("Angle: ", angle);
+            } else if(angle > 120 && angle < 170){
+                new CommandIntakeSetPivot(Intake.PivotPosition.MIDDLE);
+                telemetry.addData("Angle: ", angle);
             }
+            telemetry.update();
         }
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilities/vision/VisionUtility.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilities/vision/VisionUtility.java
@@ -1,15 +1,16 @@
 package org.firstinspires.ftc.teamcode.utilities.vision;
 
-import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
 import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
 
 import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.LLResultTypes;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.VoltageSensor;
 
 import org.firstinspires.ftc.teamcode.utilities.Geometry;
 
+import java.util.Collections;
 import java.util.List;
 
 
@@ -25,14 +26,14 @@ public class VisionUtility {
 
     /** For use outside of the utility object. Depending on how much this utility class does the
      *  user may never have to access this directly. */
-    public Limelight3A limelight;
+    public final Limelight3A limelight;
 
 
     public VisionUtility(HardwareMap map) {
         limelight = map.get(Limelight3A.class, "limelight");
-        limelight.setPollRateHz(60); // Can be polled more often but expect the camera to heat up.
+        limelight.setPollRateHz(25); // Can be polled more often but expect the camera to heat up.\
+        limelight.pipelineSwitch(0);   
         limelight.start();
-        limelight.pipelineSwitch(0);
     }
 
 
@@ -44,21 +45,27 @@ public class VisionUtility {
         if (result.getPipelineIndex() > Pipeline.RED_BLOCKS) return 0.0;
 
         List<LLResultTypes.ColorResult> colorResults = result.getColorResults();
-        List<List<Double>> corners = null;
+        List<List<Double>> corners = Collections.emptyList();
 
         for (LLResultTypes.ColorResult colorResult : colorResults) {
             // FIXME?: Will only return last colorResult from list. I am unsure if this is intended.
             corners = colorResult.getTargetCorners();
         }
 
-        Geometry.Vector2D corner1 = getCornerFromList(corners.get(0)); // may produce null?
-        Geometry.Vector2D corner2 = getCornerFromList(corners.get(1));
-        Geometry.Vector2D corner3 = getCornerFromList(corners.get(2));
+        Geometry.Vector2D corner1 = null;
+        Geometry.Vector2D corner2 = null;
+        Geometry.Vector2D corner3 = null;
+
+       if(corners.size() >= 3){
+           corner1 = getCornerFromList(corners.get(0)); // may produce null?
+           corner2 = getCornerFromList(corners.get(1));
+           corner3 = getCornerFromList(corners.get(2));
+       } else {
+           return 0.0;
+       }
 
         double l1squared = Geometry.getLengthSquared(corner1, corner2);
         double l2squared = Geometry.getLengthSquared(corner2, corner3);
-
-        telemetry.addData("Line Length 1: ", Math.sqrt(l1squared));
 
         if(l1squared > l2squared){
             return Math.toDegrees(Geometry.getAngle(corner1, corner2));
