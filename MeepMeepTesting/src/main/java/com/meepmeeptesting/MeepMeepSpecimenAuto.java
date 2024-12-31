65fa664acec2dc4b4118cf5b875c39d8cbd1120a
Andrew Bota <andrewcbota@icloud.com>
2024-12-31 11:12:42 -0800

    Moved Meep Meep Testing to its own Specimen Auto. Working on RR specimen auto pathing.

--- MeepMeepTesting/src/main/java/com/meepmeeptesting/MeepMeepSpecimenAuto.java
+++ MeepMeepTesting/src/main/java/com/meepmeeptesting/MeepMeepSpecimenAuto.java
@@ -8,24 +8,27 @@ import org.rowlandhall.meepmeep.roadrunner.DefaultBotBuilder;
 import org.rowlandhall.meepmeep.roadrunner.DriveTrainType;
 import org.rowlandhall.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
-public class MeepMeepTesting {
+public class MeepMeepSpecimenAuto {
     public static void main(String[] args) {
+        System.setProperty("sun.java2d.opengl", "true");
+
+        Pose2d startPos = new Pose2d(9, -63, Math.toRadians(180));
+        Pose2d redSpecimenMiddle = new Pose2d(0, -30);
+
+        Pose2d redPreplacedSample = new Pose2d(48, -24 + 10, Math.toRadians(90));
+
         MeepMeep meepMeep = new MeepMeep(900);
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 18.46)
                 .setDimensions(12.75, 17.75)
-                .setStartPose(new Pose2d(76, 0, Math.toRadians(90)))
                 .setDriveTrainType(DriveTrainType.MECANUM)
-                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                        .forward(9)
-                        .strafeRight(34)
-                        .splineToConstantHeading(new Vector2d(-32.25, -5.0), 0.0)
-                        .strafeLeft(5)
-                        .strafeRight(5)
-                        .splineToLinearHeading(new Pose2d(10, -30, Math.toRadians(180)), Math.toRadians(0))
-                        .strafeLeft(4)
+                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(startPos)
+                        .splineToConstantHeading(redSpecimenMiddle.vec(), Math.toRadians(180))
+                        .lineToLinearHeading(new Pose2d(36, -48, Math.toRadians(90)))
+                        .splineToConstantHeading(new Pose2d(48, -12, Math.toRadians(0)).vec(), 0)
+                        .lineToConstantHeading(new Vector2d(48, -56))
                         .build());
 
 
