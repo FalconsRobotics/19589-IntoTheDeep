beb358f61f334e26036f3bbeac614c578ca94d88
fluorescentorangepaint <49821845+Bearsmasher@users.noreply.github.com>
2025-01-06 17:08:30 -0800

    Updated Autonomous 4sample
    working 2025

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/CommandAutonomousSample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/CommandAutonomousSample.java
@@ -12,6 +12,7 @@ import org.firstinspires.ftc.teamcode.commands.CommandExtakeRotateArm;
 import org.firstinspires.ftc.teamcode.commands.CommandFollowTrajectories;
 import org.firstinspires.ftc.teamcode.commands.CommandIntakeRotateArm;
 import org.firstinspires.ftc.teamcode.commands.CommandIntakeRotateWheels;
+import org.firstinspires.ftc.teamcode.commands.CommandIntakeSetPivot;
 import org.firstinspires.ftc.teamcode.commands.CommandRun;
 import org.firstinspires.ftc.teamcode.commands.CommandTimer;
 import org.firstinspires.ftc.teamcode.subsystems.Extake;
@@ -30,7 +31,7 @@ public class CommandAutonomousSample extends CommandOpMode {
 
         autoDrive = new AutoDriveUtility(hardwareMap, sys.driveBase, new Pose2d(-40.3, -63, Math.toRadians(90)));
 
-        SequentialCommandGroup goToBucketAndUnload = new SequentialCommandGroup(
+        /* SequentialCommandGroup goToBucketAndUnload = new SequentialCommandGroup(
                 // Use directly after intakeSample.
                 new CommandIntakeRotateArm(Intake.ArmPosition.IDLE),
                 new ParallelCommandGroup(
@@ -47,43 +48,131 @@ public class CommandAutonomousSample extends CommandOpMode {
                         new CommandExtakeRotateArm(Extake.ArmPosition.LOAD),
                         new CommandExtakeMoveLift(Extake.LiftPosition.DOWN)
                 )
-        );
+        ); */
 
-        SequentialCommandGroup intakeSample = new SequentialCommandGroup(
+        /* SequentialCommandGroup intakeSample = new SequentialCommandGroup(
                 new ParallelCommandGroup(
                         new CommandIntakeRotateArm(Intake.ArmPosition.PICKUP),
                         new CommandIntakeRotateWheels(.85, 800)
                 ),
                 new CommandIntakeRotateArm(Intake.ArmPosition.UNLOAD),
                 new CommandIntakeRotateWheels(-.45, 500)
-        );
+        ); */
 
 
         schedule(new ParallelCommandGroup(
                 new SequentialCommandGroup(
-                        /*new CommandFollowTrajectories(autoDrive,
+                        // Prepare robot for placing preloaded sample
+                        new CommandIntakeRotateArm(Intake.ArmPosition.IDLE),
+                        new ParallelCommandGroup(
+                                new CommandExtakeMoveLift(Extake.LiftPosition.TOP_BUCKET),
+                                new CommandExtakeRotateArm(Extake.ArmPosition.PREPARE_UNLOAD)
+                        ),
+
+                        // Go to bucket with spline, necessary for first unload
+                        new CommandFollowTrajectories(autoDrive,
                                 autoDrive.trajectorySequenceBuilder()
                                         .splineToLinearHeading(new Pose2d(-57, -55, Math.toRadians(45)), 10)
-                        ), */
-                        goToBucketAndUnload,
+                        ),
+
+                        // Tip bucket and reset extake
+                        new CommandExtakeRotateArm(Extake.ArmPosition.UNLOAD),
+                        new CommandTimer(350),
+                        new ParallelCommandGroup(
+                                new CommandExtakeRotateArm(Extake.ArmPosition.LOAD),
+                                new CommandExtakeMoveLift(Extake.LiftPosition.DOWN)
+                        ),
+
+                        // Go to right-most ground sample and intake it
                         new CommandFollowTrajectories(autoDrive,
                                 autoDrive.trajectorySequenceBuilder()
                                         .lineToLinearHeading(new Pose2d(-48, -33, Math.toRadians(90)))
                         ),
-                        intakeSample,
-                        goToBucketAndUnload,
+                        new CommandIntakeSetPivot(0.15),
+                        new ParallelCommandGroup(
+                                new CommandIntakeRotateArm(Intake.ArmPosition.PICKUP),
+                                new CommandIntakeRotateWheels(.85, 800)
+                        ),
+                        new CommandIntakeSetPivot(0.5),
+                        new CommandIntakeRotateArm(Intake.ArmPosition.UNLOAD),
+                        new CommandIntakeRotateWheels(-.45, 500),
+                        new CommandIntakeRotateArm(Intake.ArmPosition.IDLE),
+
+                        // Prepare arm for extaking sample #2, navigate to bucket
+                        new ParallelCommandGroup(
+                                new CommandExtakeMoveLift(Extake.LiftPosition.TOP_BUCKET),
+                                new CommandExtakeRotateArm(Extake.ArmPosition.PREPARE_UNLOAD)
+                        ),
+                        new CommandFollowTrajectories(autoDrive,
+                                autoDrive.trajectorySequenceBuilder()
+                                        .lineToLinearHeading(new Pose2d(-57, -55, Math.toRadians(45)))
+                        ),
+                        new CommandExtakeRotateArm(Extake.ArmPosition.UNLOAD),
+                        new CommandTimer(350),
+                        new ParallelCommandGroup(
+                                new CommandExtakeRotateArm(Extake.ArmPosition.LOAD),
+                                new CommandExtakeMoveLift(Extake.LiftPosition.DOWN)
+                        ),
+
+                        // Drive to center ground sample, intake it
                         new CommandFollowTrajectories(autoDrive,
                                 autoDrive.trajectorySequenceBuilder()
                                         .lineToLinearHeading(new Pose2d(-58, -33, Math.toRadians(90)))
                         ),
-                        intakeSample,
-                        goToBucketAndUnload,
+                        new CommandIntakeSetPivot(0.15),
+                        new ParallelCommandGroup(
+                                new CommandIntakeRotateArm(Intake.ArmPosition.PICKUP),
+                                new CommandIntakeRotateWheels(.85, 800)
+                        ),
+                        new CommandIntakeSetPivot(0.5),
+                        new CommandIntakeRotateArm(Intake.ArmPosition.UNLOAD),
+                        new CommandIntakeRotateWheels(-.45, 500),
+                        new CommandIntakeRotateArm(Intake.ArmPosition.IDLE),
+
+                        // Navigate to bucket and extake
+                        new ParallelCommandGroup(
+                                new CommandExtakeMoveLift(Extake.LiftPosition.TOP_BUCKET),
+                                new CommandExtakeRotateArm(Extake.ArmPosition.PREPARE_UNLOAD)
+                        ),
+                        new CommandFollowTrajectories(autoDrive,
+                                autoDrive.trajectorySequenceBuilder()
+                                        .lineToLinearHeading(new Pose2d(-57, -55, Math.toRadians(45)))
+                        ),
+                        new CommandExtakeRotateArm(Extake.ArmPosition.UNLOAD),
+                        new CommandTimer(350),
+                        new ParallelCommandGroup(
+                                new CommandExtakeRotateArm(Extake.ArmPosition.LOAD),
+                                new CommandExtakeMoveLift(Extake.LiftPosition.DOWN)
+                        ),
+
+                        // Navigate to left-most grounded sample. Needs to spline because robot can't pick up the sample head-on
                         new CommandFollowTrajectories(autoDrive,
                                 autoDrive.trajectorySequenceBuilder()
                                         .splineToLinearHeading(new Pose2d(-60, -24, Math.toRadians(180)), 9)
                         ),
-                        intakeSample,
-                        goToBucketAndUnload
+                        new ParallelCommandGroup(
+                                new CommandIntakeRotateArm(Intake.ArmPosition.PICKUP),
+                                new CommandIntakeRotateWheels(.85, 800)
+                        ),
+                        new CommandIntakeRotateArm(Intake.ArmPosition.UNLOAD),
+                        new CommandIntakeRotateWheels(-.45, 500),
+                        new CommandIntakeRotateArm(Intake.ArmPosition.IDLE),
+
+                        // Go back to the bucket and EXTAKE!!!
+                        new ParallelCommandGroup(
+                                new CommandExtakeMoveLift(Extake.LiftPosition.TOP_BUCKET),
+                                new CommandExtakeRotateArm(Extake.ArmPosition.PREPARE_UNLOAD)
+                        ),
+                        new CommandFollowTrajectories(autoDrive,
+                                autoDrive.trajectorySequenceBuilder()
+                                        .lineToLinearHeading(new Pose2d(-57, -55, Math.toRadians(45)))
+                        ),
+                        new CommandExtakeRotateArm(Extake.ArmPosition.UNLOAD),
+                        new CommandTimer(350),
+                        new ParallelCommandGroup(
+                                new CommandExtakeRotateArm(Extake.ArmPosition.LOAD),
+                                new CommandExtakeMoveLift(Extake.LiftPosition.DOWN)
+                        )
                 ),
 
                 new CommandRun(() -> {
